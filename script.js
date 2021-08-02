c654a630efccf35fa166adfbc4d3f3fa13bdd01b
alexkimcs <alexkimfit@gmail.com>
2021-08-02 12:39:54 -0600

    main game function and categories

--- script.js
+++ script.js
@@ -1,16 +1,83 @@
-//Elements
-
 //fetch categories from API
-fetchCatagories(){
-    let categories = this.getCategories.map(categoryid => {
-    //asyn function method
-    return new Promise((resolve, reject) => {
-        fetch(`https://jservice.io/api/category?id=${category_id}`)
-        .then(res => res.json())
-        .then(data => {
-            resolve(data);
-            console.log(data);
+
+//categories objects array
+/*
+[
+    {
+        title:
+        clues:
+
+    }
+]
+*/
+
+
+class Game {
+    constructor(e, ids = {}) {
+        
+        this.getCategories = ids.getCategories || [17679, 25, 11743, 52, 7200];
+        this.getCategories = [17679, 25, 11743, 52, 7200];
+        //store cateegories into seperate arrays
+        this.categories = [];
+
+        this.clues = {};
+        //initiaslizes states
+        this.current = null;
+         //GET ELEMENTS
+        board = e.querySelector(".board");
+        modal = e.querySelector(".modal");
+
+    }
+
+
+    main(){
+
+    }
+    fetchCatagories(){
+        let categories = this.getCategories.map(categoryid => {
+        //asyn function method
+        return new Promise((resolve, reject) => {
+            fetch(`https://jservice.io/api/category?id=${category_id}`)
+            .then(res => res.json())
+            .then(data => {
+                resolve(data);
+                console.log(data);
+            })
+            })
+        });
+        Promise.all(categories).then(reults => {
+    
+            //loop through results category
+             
         })
-        })
-    })
+    }
 }
+
+//new instance of game
+const trivia = new Game(document.querySelector('.main'), {
+
+})
+trivia.main();
+
+
+// main(){
+
+// }
+// fetchCatagories(){
+//     let categories = this.getCategories.map(categoryid => {
+//     //asyn function method
+//     return new Promise((resolve, reject) => {
+//         fetch(`https://jservice.io/api/category?id=${category_id}`)
+//         .then(res => res.json())
+//         .then(data => {
+//             resolve(data);
+//             console.log(data);
+//         })
+//         })
+//     });
+//     Promise.all(categories).then(reults => {
+
+//         //loop through results category
+         
+//     })
+// }
