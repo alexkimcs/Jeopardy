4f5af638d6cbbfab3cce931e21268f633375fdc0
alexkimcs <alexkimfit@gmail.com>
2021-08-02 13:56:36 -0600

    working on category objects and clues

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+/*
+Sources Used:
+https://www.youtube.com/watch?v=FN_ffvw_ksE
+https://www.javascripttutorial.net/javascript-fetch-api/
+
+*/
+
 //fetch categories from API
 
 //categories objects array
@@ -24,34 +32,69 @@ class Game {
         //initiaslizes states
         this.current = null;
          //GET ELEMENTS
-        board = e.querySelector(".board");
-        modal = e.querySelector(".modal");
+        this.board = e.querySelector(".board");
+        this.modal = e.querySelector(".modal");
 
     }
 
-
+    //main function
+    //this is where everything will be called
     main(){
-
+        this.fetchCategories();
     }
-    fetchCatagories(){
-        let categories = this.getCategories.map(categoryid => {
+    //fetch data from API by category
+    fetchCategories(){
+        const categories = this.getCategories.map(category_id => {
         //asyn function method
         return new Promise((resolve, reject) => {
             fetch(`https://jservice.io/api/category?id=${category_id}`)
             .then(res => res.json())
             .then(data => {
-                resolve(data);
                 console.log(data);
+                resolve(data);
+                // console.log(data);
             })
             })
         });
-        Promise.all(categories).then(reults => {
+        Promise.all(categories).then(data => {
+            //categories objects array
+            /*
+            [
+                {
+                    title:
+                    clues:
+
+                }
+            ]
+            */
     
             //loop through results category
-             
+             data.forEach((catetgories, i) => {
+                 let categoryObj = {
+                     title: catetgories.title,
+                     clues: [] //array of clues
+                 }
+                
+                console.log(this);
+                
+                
+                 
+             })
         })
     }
 }
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+} 
+
 
 //new instance of game
 const trivia = new Game(document.querySelector('.main'), {
